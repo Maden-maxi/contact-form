9de6b0c2e596b866063a459554237483538d313c
Maden-maxi <maden.maxi@yandex.ru>
2016-11-22 17:21:49 +0200

    Complite

---
+++ app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'ngAnimate',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version',
+  'ngMaterial',
+  'ui.mask',
+  'flow'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
